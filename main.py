9921cf5aa3e721605509dd4a0735b1f22727995b
n8 ô¿ô <natewbrooks@gmail.com>
2025-10-04 21:17:42 -0400

    Add a lexeme class

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 class LexicalAnalyzer:
     def __init__(self, filepath):
+        # GIVEN
         self.filepath: str = filepath
-        # RESERVED
-        self._keywords = ["program", "true", "false", "if", "int", "str"]
-        self._symbols = [":=", ";", ":"]
         # LOOKAHEAD
-        self._lookahead: str = "" # Value of lookahead char
-        self._pos: int = -1 # Position of lookahead
-        # LEXEME
-        self._lexeme: str = "" # Value of the current lexeme string
-        self._start_pos: int = -1 # Start position of current lexeme
-        self._kind: str = "" # Type of current lookahead
-        self._line_num: int = 0 # Line number of the current lexeme
+        self.lookahead: str = "" # Value of lookahead char
+        self.pos: int = -1 # Position of lookahead
+        self.line_num: int = 0 # Line number of the lookahead
+        # LEXEMES
+        self.lexemes = []
     
     def next(self):
         with open(self.filepath, 'r') as f:
@@ -19,62 +15,47 @@ class LexicalAnalyzer:
             # if the lexeme is new, set its start position to the current position
             # keep going until the char is a whitespace (lexeme finished) or char is 
             # not approved (invalid)
-            self._pos += 1 # increment to move past current lexeme
-            self._start_pos = -1 # reset start position for current lexeme
-            self._lexeme = "" # reset lexeme
-            self._kind = ""
             
-            while self.validate(self._lookahead):
-                if(self._start_pos == -1):
-                    self._start_pos = self._pos
+            self.pos += 1 # increment to move past current lexeme
+            
+            start_pos = -1 # reset start position for current lexeme
+            lexeme = "" # reset lexeme
+            
+            while self.validate(self.lookahead):
+                if(start_pos == -1):
+                    start_pos = self.pos
                     
                 # Move cursor right once
-                f.seek(self._pos)
-                self._lookahead = f.read(1)
+                f.seek(self.pos)
+                self.lookahead = f.read(1)
                 
-                # Track new line
-                if(self._lookahead == "\n"):
-                    self._line_num += 1
-                elif(self._lookahead == "."):
-                    self._kind = "end-of-text"
+                # Track if theres a new line
+                if(self.lookahead == "\n"):
+                    self.line_num += 1
+                
+                # Ignore whitespace
+                if(self.lookahead.isspace() and lexeme.strip() == ""):
+                    start_pos = -1
+                    self.pos += 1
+                    continue
+                # When the lexeme is finished
+                elif (self.lookahead.isspace()): 
+                    self.lexemes.append(Lexeme(lexeme, start_pos, self.line_num))
+                    print(self.lexemes[-1])
                     break
                 
-                # If it is a space, lexeme is finished
-                if(self._lookahead.isspace()):
-                    print(f"{self._start_pos}:{self._line_num}:'{self.kind()}' " + self._lexeme)
-                    break
-                
-                # Increment lookahead by 1
-                self._lexeme += self._lookahead
-                self._pos += 1         
-                
-            
-        return self._lookahead
+                # Add current lookahead to the lexeme being built
+                lexeme += self.lookahead
+                self.pos += 1         
 
     def kind(self):
-        # Number
-        if self._kind == "":
-            if(self._lexeme.isdigit()):
-                self._kind = "NUM"
-            # Keyword
-            elif self._lexeme in self._keywords:
-                self._kind = self._lexeme
-            # Symbol
-            elif self._lexeme in self._symbols:
-                self._kind = self._lexeme
-            else:
-                self._kind = "ID"
-        else:
-            self._kind = "end-of-text"
-            
-
-        return self._kind
+        return self.lexemes[-1].kind
 
     def value(self):
-        pass
+        return self.lexemes[-1].value
 
     def position(self):
-        return self.position
+        return self.lexemes[-1].pos
     
     def validate(self, str):
         return True
@@ -83,17 +64,44 @@ class LexicalAnalyzer:
         return "file: " + self.filepath
         
 
+class Lexeme:
+    def __init__(self, value, pos, line_num):
+        # RESERVED
+        self.__keywords = ["program", "true", "false", "if", "int", "str"]
+        self.__symbols = [":=", ";", ":"]
+        # Info
+        self.value: str = value
+        self.kind: str = ""
+        # Position
+        self.pos: int = pos # start position
+        self.line_num: int = line_num
+        self.validate() # check the kind or return error
+        
+    def validate(self):
+        # Number
+        if(self.value.isdigit()):
+            self.kind = "NUM"
+        # Keyword
+        elif self.value in self.__keywords:
+            self.kind = self.value
+        # Symbol
+        elif self.value in self.__symbols:
+            self.kind = self.value
+        # End of file
+        elif self.value == ".":
+            self.kind == "end-of-text"
+        else:
+            self.kind = "ID"
+    
+    def __str__(self):
+        return f"{self.pos}:{self.line_num}:'{self.kind}' {self.value if (self.kind == "ID" or self.kind == "NUM") else ""}"
+            
+
 
 def main():
     filepath = parse_input_file()
     a = LexicalAnalyzer(filepath=filepath)
-    # print(a)
-    # a.next()
-    # a.kind()
-    # a.next()
-    # a.kind()
-    
-    
+
     a.next()
     # print(a.position(), a.kind(), a.value())
     while(a.kind() != 'end-of-text'):
